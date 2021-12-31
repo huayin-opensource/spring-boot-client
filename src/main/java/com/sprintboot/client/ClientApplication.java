5498e5f9c5888fb7c5e4307ab555443692e4bb99
huiyujie123 <827797174@qq.com>
2021-12-31 11:34:22 +0800

    test

--- src/main/java/com/sprintboot/client/ClientApplication.java
+++ src/main/java/com/sprintboot/client/ClientApplication.java
@@ -2,11 +2,15 @@ package com.sprintboot.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
 public class ClientApplication {
 
 	public static void main(String[] args) {
+		int taskTimestamp = 555;
+		log.info("starts run() taskTimestamp={}",taskTimestamp);
 		SpringApplication.run(ClientApplication.class, args);
 	}
 
